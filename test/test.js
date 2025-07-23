0c80f296fb2bfc5380098d8eb260f09109ebe217
Paul Rodwell <paul.rodwell@btinternet.com>
2025-07-23 14:53:36 +0100

    Order of HTML generated changed with an update.
    Rewrite tests to dissect and test the parts, and switch to using assert rather than expect.js
    Update devDependencies.

--- test/test.js
+++ test/test.js
@@ -1,6 +1,7 @@
 import { markdown } from '../src/markdown.js'
 import { describe, it } from 'node:test'
-import expect from 'expect.js'
+import assert from 'node:assert/strict'
+import { JSDOM } from 'jsdom'
 
 // inspired by https://guides.github.com/features/mastering-markdown/
 
@@ -10,33 +11,61 @@ describe('markdown plugin', () => {
 
     it('can turn ### into h3', () => {
       const result = markdown.expand('### heading')
-      expect(result).to.be('<h3>heading</h3>')
+      assert.equal('<h3>heading</h3>', result)
     })
 
     it('will turn # into h3 too', () => {
       const result = markdown.expand('# heading')
-      expect(result).to.be('<h3>heading</h3>')
+      assert.equal('<h3>heading</h3>', result)
     })
 
     it('can do ### on many lines', () => {
       const result = markdown.expand('# one\n## two')
-      expect(result).to.be('<h3>one</h3><h3>two</h3>')
+      assert.equal('<h3>one</h3><h3>two</h3>', result)
     })
   })
 
   describe('task lists', () => {
     it('can turn lines into incomplete tasks', () => {
       const result = markdown.expand('- [ ] hello world')
-      expect(result).to.be(
-        '<ul>\n<li class="task-list-item"><input data-line="1" class="task-list-item-checkbox" type="checkbox">hello world</li>\n</ul>\n',
-      )
+      const dom = new JSDOM(result)
+      // we are expecting HTML like `<ul><li class="task-list-item"><input data-line="1" class="task-list-item-checkbox" type="checkbox">hello world</li></ul>`
+      const ul = dom.window.document.querySelector('ul')
+      assert.ok(ul, 'ul element missing')
+      assert.equal(1, ul.childElementCount)
+      const li = ul.querySelector('li')
+      assert.ok(li, 'li element missing')
+      assert.equal('task-list-item', li.className)
+      assert.equal('hello world', li.textContent)
+      assert.equal(1, li.childElementCount)
+      const input = li.querySelector('input')
+      assert.ok(input, 'input element missing')
+      assert.equal('task-list-item-checkbox', input.className)
+      const inputAttrib = input.attributes
+      assert.equal('1', inputAttrib.getNamedItem('data-line').value)
+      assert.equal('checkbox', inputAttrib.getNamedItem('type').value)
+      assert.equal(null, inputAttrib.getNamedItem('checked'))
     })
 
     it('can turn lines into complete tasks', () => {
       const result = markdown.expand('- [x] hello world')
-      expect(result).to.be(
-        '<ul>\n<li class="task-list-item"><input checked="" data-line="1" class="task-list-item-checkbox" type="checkbox">hello world</li>\n</ul>\n',
-      )
+      const dom = new JSDOM(result)
+      // we are expecting HTML like `<ul><li class="task-list-item"><input checked="" data-line="1" class="task-list-item-checkbox" type="checkbox">hello world</li></ul>`,
+      const ul = dom.window.document.querySelector('ul')
+      assert.ok(ul, 'ul element missing')
+      assert.equal(1, ul.childElementCount)
+      const li = ul.querySelector('li')
+      assert.ok(li, 'li element missing')
+      assert.equal('task-list-item', li.className)
+      assert.equal('hello world', li.textContent)
+      assert.equal(1, li.childElementCount)
+      const input = li.querySelector('input')
+      assert.ok(input, 'input element missing')
+      assert.equal('task-list-item-checkbox', input.className)
+      const inputAttrib = input.attributes
+      assert.equal('1', inputAttrib.getNamedItem('data-line').value)
+      assert.equal('checkbox', inputAttrib.getNamedItem('type').value)
+      assert.ok(inputAttrib.getNamedItem('checked'))
     })
   })
 })
